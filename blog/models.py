2a0ca6360e3b591f79e1f6392d9f5f3bf296e65d
mike840609 <mike840609@gmail.com>
2018-08-06 11:22:43 +0800

    chapter 7 done

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.urls import reverse
+
+
+
 
 class Category(models.Model):
     name = models.CharField(max_length = 100)
@@ -20,18 +24,19 @@ class Tag(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=70)
     body = models.TextField()
-
     created_time = models.DateTimeField()
     modified_time = models.DateTimeField()
-
     excerpt = models.CharField(max_length=200, blank=True)
-
     category = models.ForeignKey(Category)
     tags = models.ManyToManyField(Tag, blank=True)
-
     author = models.ForeignKey(User)
 
     def __str__(self):
         return self.title
+
+    # 自定义 get_absolute_url 方法
+    # 记得从 django.urls 中导入 reverse 函数
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
 
 
