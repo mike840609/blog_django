379132c090e04d230912c8c81a5f865991d092ce
MikeTsai <mike840609@gmail.com>
2018-09-07 16:01:18 +0800

    comments feature done

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Category , Tag , Post
+from .models import Category , Tag , Post 
 
 
 class PostAdmin(admin.ModelAdmin):
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render , get_object_or_404
 from django.http import HttpResponse
-from .models import Post
+from .models import Post , Category
+from comments.forms import CommentForm
+import markdown
 
 '''
 def index(request):
@@ -18,9 +20,31 @@ def index(request):
 
 def detail(request , pk):
     post = get_object_or_404(Post, pk = pk)
-    return render(request , 'blog/detail.html' , context={ 'post':post})
+
+    post.body = markdown.markdown(post.body, extensions = [
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        'markdown.extensions.toc',
+    ] )
+
+    form = CommentForm()
+    comment_list = post.comment_set.all()
+    context = {'post': post,
+               'form': form,
+               'comment_list': comment_list
+               }
+
+    return render(request , 'blog/detail.html' , context= context)
 
 
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
     
 
-
+def category(request , pk):
+    cate = get_object_or_404(Category , pk = pk)
+    post_list = Post.objects.filter(category = cate).order_by('-created_time')
+    return render(request,'blog/index.html',context={'post_list':post_list})
